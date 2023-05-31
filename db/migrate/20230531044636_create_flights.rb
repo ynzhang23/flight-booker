038bddd07d2471c1f89c757fe88facac6b8ed8f5
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-05-31 12:57:54 +0800

    Migrate to create Airport and Flight model

--- db/migrate/20230531044636_create_flights.rb
+++ db/migrate/20230531044636_create_flights.rb
@@ -4,8 +4,8 @@ class CreateFlights < ActiveRecord::Migration[7.0]
       t.string :flight_no
       t.date :date
       t.time :time
-      t.references :depart, null: false, foreign_key: true
-      t.references :arrive, null: false, foreign_key: true
+      t.references :depart, null: false, foreign_key: { to_table: :airports }
+      t.references :arrive, null: false, foreign_key: { to_table: :airports }
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_041353) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_045137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "airports", force: :cascade do |t|
+    t.string "airport_id"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.string "flight_no"
+    t.date "date"
+    t.time "time"
+    t.bigint "depart_id", null: false
+    t.bigint "arrive_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["arrive_id"], name: "index_flights_on_arrive_id"
+    t.index ["depart_id"], name: "index_flights_on_depart_id"
+  end
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
@@ -22,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_041353) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "flights", "airports", column: "arrive_id"
+  add_foreign_key "flights", "airports", column: "depart_id"
 end
