c3e28eecbd098de6491d565bbd02cbc904c2da01
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-05-31 16:17:04 +0800

    Remove passenger and flight validation for seeding

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,8 +1,5 @@
 class Flight < ApplicationRecord
-  has_and_belongs_to_many :passengers, -> { uniq }
+  has_and_belongs_to_many :passengers
   belongs_to :depart, class_name: 'Airport'
   belongs_to :arrive, class_name: 'Airport'
-
-  # Prevent Flight from having duplicate passengers
-  validates :passenger_id, uniqueness: { scope: :flight_id }
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,3 @@
 class Passenger < ApplicationRecord
-  has_and_belongs_to_many :flights, -> { uniq }
-
-  # Prevent Passenger from having duplicate Flights
-  validates :flight_id, uniqueness: { scope: :passenger_id }
+  has_and_belongs_to_many :flights
 end
