f60a2289b21ae2c0ab12fbf8329a7c1efaded41c
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-05-31 13:25:17 +0800

    Validate duplicates for flights and passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,8 @@
 class Flight < ApplicationRecord
-  has_and_belongs_to_many :passengers
+  has_and_belongs_to_many :passengers, -> { uniq }
   belongs_to :depart, class_name: 'Airport'
   belongs_to :arrive, class_name: 'Airport'
+
+  # Prevent Flight from having duplicate passengers
+  validates :passenger_id, uniqueness: { scope: :flight_id }
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,6 @@
 class Passenger < ApplicationRecord
-  has_and_belongs_to_many :flights
+  has_and_belongs_to_many :flights, -> { uniq }
+
+  # Prevent Passenger from having duplicate Flights
+  validates :flight_id, uniqueness: { scope: :passenger_id }
 end
