3468e1f460d78d7441f42def846f6f1aee0a7964
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-05-31 13:40:39 +0800

    Create controller Flights and set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :flights
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "flights#index"
 end
